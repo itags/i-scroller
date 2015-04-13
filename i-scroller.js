8eec81930a95ae6edad370d0b4aa0e608bab7a0b
Marco Asbreuk <marco@itsasbreuk.nl>
2015-04-13 23:32:44 +0200

    intermediate saving

--- i-scroller.js
+++ i-scroller.js
@@ -8,18 +8,74 @@ module.exports = function (window) {
         itagName = 'i-scroller', // <-- define your own itag-name here
         DOCUMENT = window.document,
         ITSA = window.ITSA,
+        Event = ITSA.Event,
         IParcel = require('i-parcel')(window),
         microtemplate = require('i-parcel/lib/microtemplate.js'),
         repositionItems, Itag;
-
+/*
     repositionItems = function(element) {
-        var listsize = element.model['list-size'],
-            margeItems = Math.ceil(margePx/listsize),
+        var itemsize = element.model['item-size'],
+            margeItems = Math.ceil(margePx/itemsize),
             viewHeight = element.offsetHeight,
-            visibleItems = Math.ceil(viewHeight/listsize);
+            visibleItems = Math.ceil(viewHeight/itemsize);
     };
-
+*/
     if (!window.ITAGS[itagName]) {
+
+        Event.before('dd-drag', function(e) {
+            var node = e.target,
+                nodeHeight = node.offsetHeight,
+                iscroller = node.getParent(),
+                iscrollerHeight = iscroller.offsetHeight,
+                up, left, clientX, clientY, y, x, lasty, lastShiftedNode;
+            if (typeof e.center==='object') {
+                clientX = e.center.x;
+                clientY = e.center.y;
+            }
+            else {
+                clientX = e.clientX;
+                clientY = e.clientY;
+            }
+
+            // e.xMouse = e2.clientX;
+            up = (e.clientY>e.yMouse);
+            left = (e.clientX>e.xMouse);
+
+            if (up) {
+                lastShiftedNode = iscroller.last('span.item'+(iscroller.getData('_firstShifted') ? '.shifted' : ''), iscroller);
+                lasty = lastShiftedNode.top + lastShiftedNode.height;
+                (lasty<=(iscroller.top+iscroller.height)) && e.preventDefault();
+            }
+            else {
+                y = parseInt(node.getInlineStyle('top'), 10);
+                (y>=0) && e.preventDefault();
+            }
+        }, 'i-scroller >span');
+
+        // also: correction if dragging was too heavy and it bounced through the limit:
+        Event.after('dd-drag', function(e) {
+            var node = e.target,
+                nodeHeight = node.offsetHeight,
+                iscroller = node.getParent(),
+                iscrollerHeight = iscroller.offsetHeight,
+                y, lasty, lastShiftedNode, correction, currenty;
+            y = parseInt(node.getInlineStyle('top'), 10);
+            lastShiftedNode = iscroller.last('span.item'+(iscroller.getData('_firstShifted') ? '.shifted' : ''), iscroller);
+            lasty = lastShiftedNode.top + lastShiftedNode.height;
+            if (y>0) {
+                node.setInlineStyle('top', '0px');
+            }
+            else if (lasty<(iscroller.top+iscroller.height)) {
+                correction = iscroller.top+iscroller.height-lasty;
+                currenty = parseInt(node.getInlineStyle('top'), 10);
+                // define newy:
+                currenty += correction;
+                node.setInlineStyle('top', Math.min(0, currenty)+'px');
+            }
+            if (iscroller.getFirstItem()!==iscroller.getData('_firstItem')) {
+                iscroller.syncUI();
+            }
+        }, 'i-scroller >span');
 
         Itag = IParcel.subClass(itagName, {
             /*
@@ -29,8 +85,9 @@ module.exports = function (window) {
              * @since 0.0.1
             */
             attrs: {
-                'list-size': 'string',
+                'item-size': 'string',
                 horizontal: 'boolean',
+                'scroll-amount': 'number',
                 disabled: 'boolean',
                 'items-focusable': 'boolean',
                 scrollbar: 'boolean',
@@ -46,10 +103,12 @@ module.exports = function (window) {
                     // designNode = element.getItagContainer(),
                     model = element.model,
                     value = model.value || -1,
-                    listsize = model['list-size'] || '2em';
+                    itemsize = model['item-size'] || '2em',
+                    scrollamount = model['scroll-amount'] || 0;
 
                 element.defineWhenUndefined('value', value)
-                       .defineWhenUndefined('list-size', listsize)
+                       .defineWhenUndefined('item-size', itemsize)
+                       .defineWhenUndefined('scroll-amount', scrollamount)
                         // set the reset-value to the inital-value in case `reset-value` was not present
                        .defineWhenUndefined('reset-value', value);
 
@@ -90,43 +149,142 @@ module.exports = function (window) {
             },
 
             sync: function() {
+console.warn('sync');
                 var element = this,
                     model = element.model,
                     items = model.items,
-                    listsize = model['list-size'],
-                    template = model.template,
+                    itemsize = model['item-size'],
                     scrollContainer = element.getElement('>span'),
                     cssNode = element.getElement('>style', true),
-                    content, i, len, item, css;
+                    firstItemDrawn = element.getFirstItem(),
+                    totalItemsSize, listsizeNumber, listsizeNumberString, listsizeUnit, first, lastItemDrawn, y,
+                    totalItems, content, i, last, item, css, prevFirstItemDrawn, shiftCount, prevFirstShifted;
 
+                // start with the calculation of the current item-size in pixels: we need this
 
-                css = 'i-scroller[i-id="'+element['i-id']+'"] span.item{'+(model.horizontal ? 'width' : 'height')+':'+listsize+'}';
+                totalItems = element.getRenderedItems(firstItemDrawn);
+
+                listsizeNumber = parseInt(itemsize, 10);
+                listsizeNumberString = String(listsizeNumber);
+                listsizeUnit = itemsize.substr(listsizeNumberString.length);
+                totalItemsSize = totalItems * listsizeNumber;
+
+                css = 'i-scroller[i-id="'+element['i-id']+'"] span.item{'+(model.horizontal ? 'width' : 'height')+':'+itemsize+'}'+
+                      'i-scroller[i-id="'+element['i-id']+'"] span.item.shifted{'+(model.horizontal ? 'left' : 'top')+':'+totalItemsSize+listsizeUnit+'}';
                 cssNode.setText(css);
 
                 // building the content of the itag:
                 content = '';
-                len = items.length;
-                for (i=0; i<len; i++) {
-                    item = items[i];
-                    content += '<span class="item">';
-                    if (typeof item==='string') {
-                        content += item;
-                    }
-                    else {
-                        if (template.indexOf('<%')!==-1) {
-                            content += microtemplate(template, item);
-                        }
-                        else if (/{\S+}/.test(template)) {
-                            content += template.substitute(item);
-                        }
-                        else {
-                            content += template;
-                        }
-                    }
-                    content += '</span>';
+
+
+                lastItemDrawn = firstItemDrawn + totalItems - 1;
+
+
+
+                prevFirstItemDrawn = element.getData('_firstItem');
+                prevFirstShifted = element.getData('_firstShifted') || 0;
+
+                shiftCount = (typeof prevFirstItemDrawn==='number') ? (firstItemDrawn-prevFirstItemDrawn+prevFirstShifted) : 0;
+
+                if (shiftCount>=totalItems) {
+                    // reset the containerframe
+                    shiftCount = 0;
+                    y = parseInt(scrollContainer.getInlineStyle('top'), 10);
+                    // define newy:
+                    y += element.height;
+                    scrollContainer.setInlineStyle('top', Math.min(0, y)+'px');
                 }
+
+
+                // first draw the shifted items:
+                first = lastItemDrawn-shiftCount+1;
+                last = (shiftCount===0) ? -1 : (first+shiftCount-1);
+                // last = firstItemDrawn+totalItems+prevFirstShifted;
+console.warn('shiftCount '+shiftCount);
+console.warn('first '+first);
+console.warn('last '+last);
+
+                element.setData('_firstShifted', Math.max(0,(last-first+1)));
+                for (i=first; i<=last; i++) {
+                    item = items[i];
+                    content += element.drawItem(item, true);
+                }
+
+
+
+
+                // next, draw the non-shifted items:
+                first = firstItemDrawn;
+                last = firstItemDrawn+totalItems-1-shiftCount;
+console.warn('2. first '+first);
+console.warn('2. last '+last);
+                for (i=first; i<=last; i++) {
+                    item = items[i];
+                    content += element.drawItem(item, false);
+                }
+
+
                 // set the content:
                 scrollContainer.setHTML(content);
+                element.setData('_firstItem', firstItemDrawn);
+            },
+
+            getRenderedItems: function(firstItemDrawn) {
+                var element = this,
+                    model = element.model,
+                    items = model.items,
+                    count = 12;
+                return Math.min(count, items.length-firstItemDrawn);
+                // return this.model.items.length;
+            },
+
+            getFirstItem: function() {
+                var element = this,
+                    model = element.model,
+                    items = model.items,
+                    scrollContainer = element.getElement('>span'),
+                    shiftedPixels, firstItemNode, itemNodeSize, tempNode;
+                shiftedPixels = Math.max(0, -parseInt(scrollContainer.getInlineStyle(model.horizontal ? 'left' : 'top') || 0, 10));
+                firstItemNode = scrollContainer.getElement('>span');
+                if (firstItemNode) {
+                    itemNodeSize = firstItemNode['offset'+(model.horizontal ? 'Width' : 'Height')];
+                }
+                else {
+                    // draw dummyNode to calculate true size in pixels
+                    if (items.length>0) {
+                        tempNode = scrollContainer.append(element.drawItem(items[0], false, true));
+                        itemNodeSize = tempNode['offset'+(model.horizontal ? 'Width' : 'Height')];
+                        tempNode.remove();
+                    }
+                    else {
+                        itemNodeSize = 0;
+                    }
+                }
+console.warn('getFirstItem '+((itemNodeSize===0) ? 0 : Math.floor(shiftedPixels/itemNodeSize)));
+                return (itemNodeSize===0) ? 0 : Math.floor(shiftedPixels/itemNodeSize);
+            },
+
+            drawItem: function(oneItem, shifted, hidden) {
+                var element = this,
+                    model = element.model,
+                    template = model.template,
+                    itemContent = '<span class="item'+(shifted ? ' shifted' : '')+(hidden ? ' hidden' : '')+'">';
+                if (typeof oneItem==='string') {
+                    itemContent += oneItem;
+                }
+                else {
+                    if (template.indexOf('<%')!==-1) {
+                        itemContent += microtemplate(template, oneItem);
+                    }
+                    else if (/{\S+}/.test(template)) {
+                        itemContent += template.substitute(oneItem);
+                    }
+                    else {
+                        itemContent += template;
+                    }
+                }
+                itemContent += '</span>';
+                return itemContent;
             },
 
             destroy: function() {
